2f38b704e13aaa442c9042a2639229cd35a5b9c8
Camila Alm <camila.salmeidatech@hotmail.com>
2024-09-25 01:59:07 -0400

    setup and express

---
+++ server.js
@@ -0,0 +1,22 @@
+//Import modules
+const express = require('express')
+
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(4000, () => {
+    console.log("Listening on port 4000 🎧!")
+})
+
